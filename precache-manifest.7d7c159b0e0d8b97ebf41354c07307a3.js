1d0a9b5bdba42431593ae2b68d8459d19482e106
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-05-15 20:10:04 +0100

    Updates

---
+++ precache-manifest.7d7c159b0e0d8b97ebf41354c07307a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b58d356d1b4111764ec03ad5231f465",
+    "url": "/appointments-booker-react/index.html"
+  },
+  {
+    "revision": "550b6930eb6316ed8a06",
+    "url": "/appointments-booker-react/static/js/2.be335135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appointments-booker-react/static/js/2.be335135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192e0be4fbe163809454",
+    "url": "/appointments-booker-react/static/js/main.299f942d.chunk.js"
+  },
+  {
+    "revision": "5549cb05b131b06f48e3",
+    "url": "/appointments-booker-react/static/js/runtime-main.530e27f6.js"
+  }
+]);
